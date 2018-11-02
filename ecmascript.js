8af3516df25cf40e0774f5a41f59f56c020cd3b3
lana <iloveitsdev@gmail.com>
2018-11-02 16:23:58 -0400

    Create ecmascript.js (#1)

---
+++ ecmascript.js
@@ -0,0 +1,23 @@
+class Animal {
+  constructor(name, energy) {
+    this.name = name
+    this.energy = energy
+  }
+  eat(amount) {
+    console.log(`${this.name} is eating.`)
+    this.energy += amount
+  }
+  sleep(length) {
+    console.log(`${this.name} is sleeping.`)
+    this.energy += length
+  }
+  play(length) {
+    console.log(`${this.name} is playing.`)
+    this.energy -= length
+  }
+}
+let name = document.getElementById("name");
+let energy = document.getElementById("energy");
+
+const newAnimal = new Animal(name, enegry)
+ 
